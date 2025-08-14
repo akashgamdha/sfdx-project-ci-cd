f24d3f86eb9df4e7e27087245eed0b04872aae2d
Gamdha <akashgamdha@kpmg.com>
2025-08-14 11:18:08 +0530

    Demo LWC connectedCallback added

--- force-app/main/default/lwc/demoLWC/demoLWC.js
+++ force-app/main/default/lwc/demoLWC/demoLWC.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class DemoLWC extends LightningElement {}
+export default class DemoLWC extends LightningElement {
+    connectedCallback() {
+        console.log('Demo callback');
+    }
+}
