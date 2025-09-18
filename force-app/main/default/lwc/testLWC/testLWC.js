4efe57a5a7df962ef227e8852b36bba4e7181b1c
akashgamdha <109716449+akashgamdha@users.noreply.github.com>
2025-09-18 21:35:10 +0530

    Update testLWC.js

--- force-app/main/default/lwc/testLWC/testLWC.js
+++ force-app/main/default/lwc/testLWC/testLWC.js
@@ -3,5 +3,6 @@ import { LightningElement } from 'lwc';
 export default class TestLWC extends LightningElement {
   connectedCallback() {
     console.log('Hi');
+    console.log('Another Hi');
   }
 }
